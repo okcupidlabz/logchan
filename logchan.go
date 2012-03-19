e705fb99f5d4bf8d43ba6e2a6aef15e0afce0f8a
Maxwell Krohn <themax@gmail.com>
2012-03-19 17:40:42 -0400

    export the fields

--- logchan.go
+++ logchan.go
@@ -10,16 +10,18 @@ import (
 type Level uint64
 
 type Channel struct {
-	level Level
-	key byte
-	desc string
+	Level Level
+	Key byte
+	Desc string
 }
 
 type Channels []Channel
 
 type Logger struct {
 	level Level
+	channels Channels
 	chanmap map[byte]Channel
+	bitmap map[Level]Channel
 }
 
 func NewLogger(ch Channels, def Level) *Logger {
@@ -27,14 +29,39 @@ func NewLogger(ch Channels, def Level) *Logger {
 	ret.level = def
 	ret.chanmap = make(map[byte]Channel)
 	for _, c := range ch {
-		ret.chanmap[c.key] = c
+		ret.chanmap[c.Key] = c
+		ret.bitmap[c.Level] = c
 	}
+	ret.channels = ch
 	return ret
 }
 
 
 func (logger *Logger) AtLevel (l Level) bool {
-	return (l & logger.level) == l
+	return (l & logger.level) != 0
+}
+
+func (logger *Logger) LevelToPrefix (l Level) string {
+	s := logger.LevelToString(l)
+	if len(s) > 0 {
+		s = fmt.Sprintf("[%s] ", s)
+	}
+	return s
+}
+
+func (logger *Logger) LevelToString(l Level) string {
+	descs := make([]string,len(logger.channels))
+	
+	
+	for i := 0; l != 0 && i < len(logger.channels); i++ {
+		c := logger.channels[i]
+		if (c.Level & l) != 0 {
+			descs = append (descs, c.Desc)
+			l = l&(^c.Level)
+		}
+	}
+
+	return strings.Join(descs, ",")
 }
 
 func (logger *Logger) SetChannels (s string) (e error, newdesc string) {
@@ -42,12 +69,9 @@ func (logger *Logger) SetChannels (s string) (e error, newdesc string) {
 	var newlev Level = 0
 
 
-	descs := make([]string,len(s))
-
 	for _, c := range []byte(s) {
 		if ch, found := logger.chanmap[c]; found {
-			newlev |= ch.level
-			descs = append (descs, ch.desc)
+			newlev |= ch.Level
 		} else {
 			e = fmt.Errorf("bad logger channel found: '%c'\n", c)
 			break
@@ -55,7 +79,7 @@ func (logger *Logger) SetChannels (s string) (e error, newdesc string) {
 	}
 
 	if e == nil {
-		newdesc = strings.Join(descs, ",")
+		newdesc = logger.LevelToString(newlev)
 		logger.level = newlev
 	}
 	return
@@ -64,18 +88,30 @@ func (logger *Logger) SetChannels (s string) (e error, newdesc string) {
 
 func (logger *Logger) Printf(l Level, fmt string, v ...interface{}) {
 	if logger.AtLevel (l) {
+		s := logger.LevelToPrefix (l)
+		if len(s) > 0 {
+			fmt = s + fmt
+		}
 		log.Printf (fmt, v...)
 	}
 }
 
 func (logger *Logger) Print(l Level, v ...interface{}) {
 	if logger.AtLevel (l) {
-		log.Print (v...)
+		s := logger.LevelToPrefix (l)
+		if len(s) > 0 {
+			log.Print (s)
+		}
+		log.Print(v)
 	}
 }
 
 func (logger *Logger) Println(l Level, v ...interface{}) {
 	if logger.AtLevel (l) {
+		s := logger.LevelToPrefix (l)
+		if len(s) > 0 {
+			log.Print (s)
+		}
 		log.Println (v...)
 	}
 }
